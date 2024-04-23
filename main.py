e5dfb3fb5ce710baa34d4fa4d92baa7232836be8
david123456858 <juandavidperaltafuentes@gmail.com>
2024-04-22 19:59:42 -0500

    cositas

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI,UploadFile,File,Response,status,Body
 from fastapi.responses import UJSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
-import os
-import json
+
 
 from src.controller.sensor import read_file,generateWAndU,saveValues,sensor_data,generateWandUforCapas
 from src.upload.save_file import data_root
@@ -24,7 +23,7 @@ app.add_middleware(
 
 @app.get('/')
 def read_root():
-    return {"info":"Bienvenido a nuestrp back del sensor"}
+    return {"info":"Bienvenido a nuestro back del sensor"}
 
 ##Post donde se nos permite traer el archivo de entrenamiento y hacer el debido proceso de inializacion 
 @app.post('/file',status_code=202,response_class=UJSONResponse)
@@ -45,8 +44,6 @@ async def recive_file(res:Response,file:UploadFile = File()):
                   "cabeceras":cabeceras,
                   "salidas":y_columns.values.tolist(),
                   "entradas":x_columns.values.tolist(),}]
-
-
 @app.post('/simular',status_code=202,response_class=UJSONResponse)
 async def recive_file(res:Response,file:UploadFile = File()):
     if(file):
@@ -56,7 +53,6 @@ async def recive_file(res:Response,file:UploadFile = File()):
         x_columns = df.filter(like='X')
         return [{ 
                   "entradas":x_columns.values.tolist(),}]
-
 ##subiendo el arhivo
 @app.post("/save",status_code=200,response_class=UJSONResponse)
 def saveW_U(values_data:sensor):
@@ -65,10 +61,6 @@ def saveW_U(values_data:sensor):
     df = sensor_data(values_data)
     print(df)
     df.to_excel(_path, index=False)
-    # dfW,dfU = saveValues(values_data)
-    # with pd.ExcelWriter(_path) as writer:
-    #     dfW.to_excel(writer,sheet_name='Hoja1', index=False)
-    #     dfU.to_excel(writer,sheet_name='Hoja2',index=False)
     return {"Los valores se han guardado correctamente"}
       
 @app.post("/capas",status_code=200, response_class=UJSONResponse)
