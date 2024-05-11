f6afd9b7d7d42963f6c039f837052f1520866253
david123456858 <juandavidperaltafuentes@gmail.com>
2024-05-10 21:02:39 -0500

    Banco cambiados

--- src/controller/map.py
+++ src/controller/map.py
@@ -1,12 +1,12 @@
 mapeoBanco = {
-"cuantia":{"baja":0,"media":1,"alta":11},
+"cuantia":{"baja":1,"media":2,"alta":3},
 "vivienda":{"propiedad":0,"alquiler":1},
 "trabajo":{"sí":1,"no":0},
 "credito":{"concedido":1,"denegado":0}
             }
 
 mapeoOptica = {
-"Edad":{"joven":0,"pre-presbicia":1,"presbicia":11},
+"Edad":{"joven":1,"pre-presbicia":2,"presbicia":3},
 "Anomalía":{"miopía":0,"hipermetropía":1},
 "Astigmatismo":{"sí":1,"no":0},
 "Lentes de contacto":{"duras":1,"blandas":0}
